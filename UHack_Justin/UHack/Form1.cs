0e5cd16cdc09178bf342c8994a2c772b49978dc4
BruteForceCode <justin2348@juno.com>
2014-09-27 06:09:54 -0500

    not working database connections
    my epic failure of sql

--- UHack_Justin/UHack/Form1.cs
+++ UHack_Justin/UHack/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,13 +16,14 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
 
-            firstName.Text = Properties.Settings.Default.firstName;
-            lastName.Text = Properties.Settings.Default.lastName;
+            username.Text = Properties.Settings.Default.firstName;
+            password.Text = Properties.Settings.Default.lastName;
         }
 
         private void createAccount_Click(object sender, EventArgs e)
         {
-            if (firstName.Text.Length == 0 || lastName.Text.Length == 0)
+            /*
+            if (username.Text.Length == 0 || password.Text.Length == 0)
             {
                 //message box when no names
                 MessageBox.Show("Please enter a first name and last name.");
@@ -29,8 +31,8 @@ namespace WindowsFormsApplication1
             else
             {
                 //set first and last names
-                Properties.Settings.Default.firstName = firstName.Text;
-                Properties.Settings.Default.lastName = lastName.Text;
+                Properties.Settings.Default.firstName = username.Text;
+                Properties.Settings.Default.lastName = password.Text;
                 Properties.Settings.Default.Save();
 
                 //create main form
@@ -38,7 +40,76 @@ namespace WindowsFormsApplication1
                 Form2 f2 = new Form2();
                 f2.ShowDialog();
             }
+             */
+            //send you to new user form
+            this.Hide();
+            NewUser user = new NewUser(username.Text, password.Text);
+            user.ShowDialog();
 
+        }
+
+        private void firstLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void username_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            if (username.Text.Length == 0 || password.Text.Length == 0)
+            {
+                //message box when no names
+                MessageBox.Show("Please enter a username and password.");
+            }
+            else
+            {
+                //set first and last names
+                //Properties.Settings.Default.firstName = username.Text;
+                //Properties.Settings.Default.lastName = password.Text;
+                //Properties.Settings.Default.Save();
+
+
+                try
+                {
+                    String connString = "Data Source=localhost;Initial Catalog=Stance;";
+                    SqlConnection conn = new SqlConnection(connString);
+                    //SqlCommand cmd = new SqlCommand(query, conn);
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = "";
+
+                    /*
+                    // Create a new adapter and give it a query to fetch sales order, contact,  
+                    // address, and product information for sales in the year 2002. Point connection  
+                    // information to the configuration setting "AdventureWorks".
+                    //string connectionString = "Data Source=localhost;Initial Catalog=AdventureWorks;"
+                    //    + "Integrated Security=true;";
+                    System.Data.SqlClient.SqlConnectionStringBuilder builder =
+                    new System.Data.SqlClient.SqlConnectionStringBuilder();
+                    builder["Data Source"] = "(local)";
+                    builder["integrated Security"] = true;
+                    builder["Initial Catalog"] = "AdventureWorks;NewValue=Bad";
+                    Console.WriteLine(builder.ConnectionString);
+
+                    MessageBox.Show("");
+                    
+                    //SqlDataAdapter da = new SqlDataAdapter(connectionString);
+                     */
+
+                } catch (SqlException ex) {
+                    Console.WriteLine("SQL exception occurred: " + ex.Message);
+                }
+                
+                //create main form
+                this.Hide();
+                Form2 f2 = new Form2();
+                f2.ShowDialog();
+            }
         }
 
     }
--- UHack_Justin/UHack/NewUser.Designer.cs
+++ UHack_Justin/UHack/NewUser.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApplication1
 {
-    partial class entryForm
+    partial class NewUser
     {
         /// <summary>
         /// Required designer variable.
@@ -32,9 +32,8 @@
             this.firstLabel = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.createAccount = new System.Windows.Forms.Button();
-            this.username = new System.Windows.Forms.TextBox();
-            this.password = new System.Windows.Forms.TextBox();
-            this.login = new System.Windows.Forms.Button();
+            this.firstName = new System.Windows.Forms.TextBox();
+            this.lastName = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // titleLabel
@@ -57,9 +56,9 @@
             this.firstLabel.ForeColor = System.Drawing.Color.White;
             this.firstLabel.Location = new System.Drawing.Point(48, 102);
             this.firstLabel.Name = "firstLabel";
-            this.firstLabel.Size = new System.Drawing.Size(106, 27);
+            this.firstLabel.Size = new System.Drawing.Size(111, 27);
             this.firstLabel.TabIndex = 1;
-            this.firstLabel.Text = "Username";
+            this.firstLabel.Text = "First Name";
             this.firstLabel.Click += new System.EventHandler(this.firstLabel_Click);
             // 
             // label1
@@ -70,53 +69,38 @@
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(51, 145);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 27);
+            this.label1.Size = new System.Drawing.Size(108, 27);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Password";
+            this.label1.Text = "Last Name";
             // 
             // createAccount
             // 
             this.createAccount.BackColor = System.Drawing.Color.Maroon;
             this.createAccount.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.createAccount.ForeColor = System.Drawing.Color.White;
-            this.createAccount.Location = new System.Drawing.Point(189, 197);
+            this.createAccount.Location = new System.Drawing.Point(85, 197);
             this.createAccount.Name = "createAccount";
-            this.createAccount.Size = new System.Drawing.Size(160, 45);
+            this.createAccount.Size = new System.Drawing.Size(202, 45);
             this.createAccount.TabIndex = 3;
             this.createAccount.Text = "Create Account";
             this.createAccount.UseVisualStyleBackColor = false;
             this.createAccount.Click += new System.EventHandler(this.createAccount_Click);
             // 
-            // username
+            // firstName
             // 
-            this.username.BackColor = System.Drawing.Color.White;
-            this.username.Location = new System.Drawing.Point(175, 108);
-            this.username.Name = "username";
-            this.username.Size = new System.Drawing.Size(134, 20);
-            this.username.TabIndex = 4;
-            this.username.TextChanged += new System.EventHandler(this.username_TextChanged);
+            this.firstName.BackColor = System.Drawing.Color.White;
+            this.firstName.Location = new System.Drawing.Point(175, 108);
+            this.firstName.Name = "firstName";
+            this.firstName.Size = new System.Drawing.Size(134, 20);
+            this.firstName.TabIndex = 4;
             // 
-            // password
+            // lastName
             // 
-            this.password.BackColor = System.Drawing.Color.White;
-            this.password.Location = new System.Drawing.Point(175, 151);
-            this.password.Name = "password";
-            this.password.Size = new System.Drawing.Size(134, 20);
-            this.password.TabIndex = 5;
-            this.password.UseSystemPasswordChar = true;
-            // 
-            // login
-            // 
-            this.login.BackColor = System.Drawing.Color.Maroon;
-            this.login.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.login.ForeColor = System.Drawing.Color.White;
-            this.login.Location = new System.Drawing.Point(23, 197);
-            this.login.Name = "login";
-            this.login.Size = new System.Drawing.Size(160, 45);
-            this.login.TabIndex = 6;
-            this.login.Text = "Login";
-            this.login.UseVisualStyleBackColor = false;
-            this.login.Click += new System.EventHandler(this.login_Click);
+            this.lastName.BackColor = System.Drawing.Color.White;
+            this.lastName.Location = new System.Drawing.Point(175, 151);
+            this.lastName.Name = "lastName";
+            this.lastName.Size = new System.Drawing.Size(134, 20);
+            this.lastName.TabIndex = 5;
             // 
             // entryForm
             // 
@@ -125,9 +109,8 @@
             this.BackColor = System.Drawing.Color.ForestGreen;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(372, 272);
-            this.Controls.Add(this.login);
-            this.Controls.Add(this.password);
-            this.Controls.Add(this.username);
+            this.Controls.Add(this.lastName);
+            this.Controls.Add(this.firstName);
             this.Controls.Add(this.createAccount);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.firstLabel);
@@ -146,9 +129,8 @@
         private System.Windows.Forms.Label firstLabel;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button createAccount;
-        private System.Windows.Forms.TextBox username;
-        private System.Windows.Forms.TextBox password;
-        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.TextBox firstName;
+        private System.Windows.Forms.TextBox lastName;
     }
 }
 
